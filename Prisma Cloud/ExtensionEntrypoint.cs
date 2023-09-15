7df2435c9c9aa5232fb784f547c936d8acff6e77
dneprix <dneprix@gmail.com>
2023-09-15 14:55:32 +0100

    init

---
+++ Prisma Cloud/ExtensionEntrypoint.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.Extensibility;
+
+namespace Prisma_Cloud
+{
+    /// <summary>
+    /// Extension entrypoint for the VisualStudio.Extensibility extension.
+    /// </summary>
+    [VisualStudioContribution]
+    internal class ExtensionEntrypoint : Extension
+    {
+        /// <inheritdoc/>
+        public override ExtensionConfiguration ExtensionConfiguration => new()
+        {
+            Metadata = new("Prisma_Cloud.460298fb-bab1-4c92-a97f-e6ccd50277be", this.ExtensionAssemblyVersion, "Publisher name", "Prisma_Cloud"),
+        };
+
+        /// <inheritdoc />
+        protected override void InitializeServices(IServiceCollection serviceCollection)
+        {
+            base.InitializeServices(serviceCollection);
+
+            // You can configure dependency injection here by adding services to the serviceCollection.
+        }
+    }
+}
